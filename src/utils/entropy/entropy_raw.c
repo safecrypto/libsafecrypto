aff1202525045c27ae2d04c20be00e9e23acd9c3
loisscott <33118577+loisscott@users.noreply.github.com>
2017-12-19 14:38:23 +0000

    Update entropy_raw.c
    Made use of tabs & spaces to be consistent as the mix made it difficult to read the indentation...

--- src/utils/entropy/entropy_raw.c
+++ src/utils/entropy/entropy_raw.c
@@ -20,7 +20,7 @@
 
 SINT32 encode_raw_32(sc_packer_t *packer, size_t n, const SINT32 *p, size_t bits)
 {
-	size_t i;
+    size_t i;
 
     // Write bits for each symbol to the stream
     for (i=0; i<n; i++) {
@@ -34,12 +34,12 @@ SINT32 encode_raw_32(sc_packer_t *packer, size_t n, const SINT32 *p, size_t bits
 
 SINT32 decode_raw_signed_32(sc_packer_t *packer, size_t n, SINT32 *p, size_t bits)
 {
-	size_t i;
+    size_t i;
     UINT32 sign, sign_extension, value;
     sign           = 1 << (bits - 1);
     sign_extension = ((1 << (32 - bits)) - 1) << bits;
 
-   	for (i=0; i<n; i++) {
+    for (i=0; i<n; i++) {
        	// Obtain the sign bit, create a mask for sign extension and read the raw bits
        	if (SC_FUNC_FAILURE == packer->read(packer, &value, bits)) {
             return SC_ERROR;
@@ -52,12 +52,12 @@ SINT32 decode_raw_signed_32(sc_packer_t *packer, size_t n, SINT32 *p, size_t bit
 
 SINT32 decode_raw_unsigned_32(sc_packer_t *packer, size_t n, SINT32 *p, size_t bits)
 {
-	size_t i;
+    size_t i;
     UINT32 value;
 
     // Iterate through each symbol
-  	for (i=0; i<n; i++) {
-   		if (SC_FUNC_FAILURE == packer->read(packer, &value, bits)) {
+    for (i=0; i<n; i++) {
+        if (SC_FUNC_FAILURE == packer->read(packer, &value, bits)) {
             return SC_ERROR;
        	}
        	p[i] = value;
@@ -68,7 +68,7 @@ SINT32 decode_raw_unsigned_32(sc_packer_t *packer, size_t n, SINT32 *p, size_t b
 
 SINT32 encode_raw_16(sc_packer_t *packer, size_t n, const SINT16 *p, size_t bits)
 {
-	size_t i;
+    size_t i;
 
     // Write bits for each symbol to the stream
     for (i=0; i<n; i++) {
@@ -82,12 +82,12 @@ SINT32 encode_raw_16(sc_packer_t *packer, size_t n, const SINT16 *p, size_t bits
 
 SINT32 decode_raw_signed_16(sc_packer_t *packer, size_t n, SINT16 *p, size_t bits)
 {
-	size_t i;
+    size_t i;
     UINT32 sign, sign_extension, value;
     sign           = 1 << (bits - 1);
     sign_extension = ((1 << (16 - bits)) - 1) << bits;
 
-   	for (i=0; i<n; i++) {
+    for (i=0; i<n; i++) {
        	// Obtain the sign bit, create a mask for sign extension and read the raw bits
        	if (SC_FUNC_FAILURE == packer->read(packer, &value, bits)) {
             return SC_ERROR;
@@ -100,12 +100,12 @@ SINT32 decode_raw_signed_16(sc_packer_t *packer, size_t n, SINT16 *p, size_t bit
 
 SINT32 decode_raw_unsigned_16(sc_packer_t *packer, size_t n, SINT16 *p, size_t bits)
 {
-	size_t i;
+    size_t i;
     UINT32 value;
 
     // Iterate through each symbol
-  	for (i=0; i<n; i++) {
-   		if (SC_FUNC_FAILURE == packer->read(packer, &value, bits)) {
+    for (i=0; i<n; i++) {
+        if (SC_FUNC_FAILURE == packer->read(packer, &value, bits)) {
             return SC_ERROR;
        	}
        	p[i] = value;
